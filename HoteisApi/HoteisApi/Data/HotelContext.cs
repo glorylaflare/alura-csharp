bf576fd73a0b73917fa62daa2e8ffea0e4aee490
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-07-13 17:28:08 -0300

    feat: adicionar contexto do banco de dados para o modelo Hotel

---
+++ HoteisApi/HoteisApi/Data/HotelContext.cs
@@ -0,0 +1,13 @@
+﻿using HoteisApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HoteisApi.Data;
+
+public class HotelContext : DbContext
+{
+    public HotelContext(DbContextOptions<HotelContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Hotel> Hoteis { get; set; } // DbSet for the Hotel model
+}

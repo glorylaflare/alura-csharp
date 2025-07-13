d041cd24a562eee7daaf8418a157e3edf1428287
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-07-13 17:28:25 -0300

    feat: adicionar perfil de mapeamento para DTOs de Hotel

---
+++ HoteisApi/HoteisApi/Profiles/HotelProfile.cs
@@ -0,0 +1,16 @@
+﻿using HoteisApi.Data.Dtos;
+using HoteisApi.Models;
+using AutoMapper;
+
+namespace HoteisApi.Profiles;
+
+public class HotelProfile : Profile
+{
+    public HotelProfile()
+    {
+        CreateMap<CreateHotelDto, Hotel>();
+        CreateMap<UpdateHotelDto, Hotel>();
+        CreateMap<Hotel, UpdateHotelDto>();
+        CreateMap<Hotel, ReadHotelDto>();
+    }
+}

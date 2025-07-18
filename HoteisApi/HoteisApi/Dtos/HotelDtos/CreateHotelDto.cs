dd9110c9f7299034694df82efeb46288ef5c0f4a
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-07-17 22:00:34 -0300

    feat: adicionar DTOs para criação e atualização de hotéis com propriedades relevantes

---
+++ HoteisApi/HoteisApi/Dtos/HotelDtos/CreateHotelDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HoteisApi.Data.Dtos;
+
+public class CreateHotelDto
+{
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public string Telefone { get; set; }
+    public int Estrelas { get; set; }
+    public bool TemWifi { get; set; }
+    public bool TemEstacionamento { get; set; }
+    public bool AceitaAnimais { get; set; }
+    public bool TemPiscina { get; set; }
+    public bool TemAcademia { get; set; }
+    public bool TemRestaurante { get; set; }
+    
+    public EnderecoDto Endereco { get; set; }
+}
---
+++ HoteisApi/HoteisApi/Dtos/HotelDtos/ReadHotelDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HoteisApi.Data.Dtos;
+
+public record ReadHotelDto(
+    string Nome,
+    string Descricao,
+    string Telefone,
+    int Estrelas,
+    bool TemWifi,
+    bool TemEstacionamento,
+    bool AceitaAnimais,
+    bool TemPiscina,
+    bool TemAcademia,
+    bool TemRestaurante,
+    EnderecoDto Endereco);

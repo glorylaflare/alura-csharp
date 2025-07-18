766aed4a08501be01704db627e3fac0eaf41e840
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-07-17 22:00:30 -0300

    feat: adicionar DTOs para criação, leitura e atualização de clientes com propriedades relevantes

---
+++ HoteisApi/HoteisApi/Dtos/ClienteDtos/CreateClienteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HoteisApi.Data.Dtos;
+
+public class CreateClienteDto
+{
+    public int Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+    public string Documento { get; set; }
+}
---
+++ HoteisApi/HoteisApi/Dtos/ClienteDtos/ReadClienteDto.cs
@@ -0,0 +1,11 @@
+﻿using HoteisApi.Models;
+
+namespace HoteisApi.Data.Dtos;
+
+public record ReadClienteDto(
+    int Id,
+    int Nome,
+    string Email,
+    string Telefone,
+    string Documento,
+    ICollection<Reserva> Reservas);

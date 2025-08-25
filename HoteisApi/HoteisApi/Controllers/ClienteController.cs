a89d34d2ab928ecb6d6c5217e58d08d728a7f31a
glorylaflare <marcelojunior_@outlook.com.br>
2025-08-25 08:52:25 -0300

    feat: criação do controller de cliente

--- HoteisApi/HoteisApi/Controllers/ClienteController.cs
+++ HoteisApi/HoteisApi/Controllers/ClienteController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using HoteisApi.Data;
+using HoteisApi.Data.Dtos;
+using HoteisApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HoteisApi.Controllers;
 
@@ -6,5 +11,35 @@ namespace HoteisApi.Controllers;
 [Route("api/v1/[controller]")] // Route to access the controller
 public class ClienteController : ControllerBase
 {
-    
+    private readonly HotelContext _hotelContext;
+    private readonly IMapper _mapper;
+
+    public ClienteController(HotelContext hotelContext, IMapper mapper)
+    {
+        _hotelContext = hotelContext;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public ActionResult AdicionarCliente([FromBody] CreateClienteDto clienteDto)
+    {
+        var cliente = _mapper.Map<Cliente>(clienteDto);
+        _hotelContext.Clientes.Add(cliente);
+        _hotelContext.SaveChanges();
+        Console.WriteLine($"Cliente {cliente.Nome} adicionado com sucesso!");
+        return CreatedAtAction(nameof(RecuperaClientePorId), new { id = cliente.Id }, clienteDto);
+    }
+
+    [HttpGet]
+
+    [HttpGet("{id}")]
+    /*
+    public ActionResult<Cliente> RecuperaClientePorId(int id)
+    {
+        var cliente 
+    }
+    */
+    [HttpPut]
+
+    [HttpDelete]
 }
